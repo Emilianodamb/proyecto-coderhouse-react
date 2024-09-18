20af4247a7f686fa104a9451cb431e5722c1ba97
Emilianodamb <emilianolrc@gmail.com>
2024-09-18 15:29:16 -0300

    Inicializar servidor, eliminar codigo predefinido, repositorio y arbol de archivos listo para empezar a trabajar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Primer commit - inicializando el proyecto</h1>
+      <h2>Levantando servidor y eliminando el código predefinido</h2>
+    </>
+  )
+}
+
+export default App
